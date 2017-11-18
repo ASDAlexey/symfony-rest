23fdbaeb53193ef57ebce80fd8f7af417f75b41f
Alexey Popov <alexey.popov@umbrella-web.com>
2017-11-18 22:50:38 +0300

    made basic product post endpoint

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use AppBundle\Form\ProductFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -23,5 +24,52 @@ class ProductController extends BaseController {
     ], 200);
 
     return $response;
+  }
+
+  /**
+   * @Route("/api/products", name="product_new")
+   */
+  public function newAction(Request $request) {
+    $form = $this->createForm(ProductFormType::class);
+    $form->submit($request->request->all());
+
+    if ($form->isValid()) {
+      $formData = $form->getData();
+      $responseData = [
+        "data" => $formData,
+      ];
+      return $this->createApiResponse($responseData);
+    } else{
+      $errors = $form->getErrors()->getForm();
+      $responseData = ["errors" => $errors];
+      return $this->createApiResponse($responseData, 400);
+    }
+//    // only handles data on POST
+//    $form->handleRequest($request);
+//
+//    if ($form->isSubmitted() && $form->isValid()) {
+//      $product = $form->getData();
+//
+//      // $file stores the uploaded file
+//      $file = $product->getImage();
+//
+//      $fileName = $this->get('app.image')->move($file);
+//
+//      // Update the 'avatar' property to store file name
+//      // instead of its contents
+//      $product->setImage($fileName);
+//
+//      $product->setUser($this->getUser());
+//
+//      $em = $this->getDoctrine()->getManager();
+//      $em->persist($product);
+//      $em->flush();
+//
+//      $this->addFlash('success', 'Product created');
+//
+//      return $this->redirectToRoute('product_list');
+//    }
+//
+//    return $this->render('product/edit.html.twig', ['productForm' => $form->createView(), 'imageName' => null]);
   }
 }
