513afba38f82f5d76d73edd44739366798d942b3
Alexey Popov <alexey.popov@umbrella-web.com>
2017-11-19 17:32:30 +0300

    made create new product endpoint without image

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -2,13 +2,11 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\User;
+use AppBundle\Entity\Product;
 use AppBundle\Form\ProductFormType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use AppBundle\Controller\BaseController;
 
 class ProductController extends BaseController {
   /**
@@ -35,41 +33,68 @@ class ProductController extends BaseController {
 
     if ($form->isValid()) {
       $formData = $form->getData();
+      //      // Update the 'avatar' property to store file name
+      //      // instead of its contents
+      //      $product->setImage($fileName);
+
+      /**
+       * @var Product $product
+       */
+      $product = new Product();
+      $product->setName($formData->getName());
+      $product->setPrice($formData->getPrice());
+      $product->setDescription($formData->getDescription());
+      $product->setColor($formData->getColor());
+      $product->setYear($formData->getYear());
+      // $file stores the uploaded file
+      //      $file = $product->getImage();
+      //      $fileName = $this->get('app.image')->move($file);
+      //      // Update the 'avatar' property to store file name
+      //      // instead of its contents
+      //      $product->setImage($fileName);
+
+      $product->setUser($this->getUser());
+      $product->setCreatedAt();
+
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($product);
+      $em->flush();
+
       $responseData = [
-        "data" => $formData,
+        "data" => $product,
       ];
       return $this->createApiResponse($responseData);
-    } else{
+    } else {
       $errors = $form->getErrors()->getForm();
       $responseData = ["errors" => $errors];
       return $this->createApiResponse($responseData, 400);
     }
-//    // only handles data on POST
-//    $form->handleRequest($request);
-//
-//    if ($form->isSubmitted() && $form->isValid()) {
-//      $product = $form->getData();
-//
-//      // $file stores the uploaded file
-//      $file = $product->getImage();
-//
-//      $fileName = $this->get('app.image')->move($file);
-//
-//      // Update the 'avatar' property to store file name
-//      // instead of its contents
-//      $product->setImage($fileName);
-//
-//      $product->setUser($this->getUser());
-//
-//      $em = $this->getDoctrine()->getManager();
-//      $em->persist($product);
-//      $em->flush();
-//
-//      $this->addFlash('success', 'Product created');
-//
-//      return $this->redirectToRoute('product_list');
-//    }
-//
-//    return $this->render('product/edit.html.twig', ['productForm' => $form->createView(), 'imageName' => null]);
+    //    // only handles data on POST
+    //    $form->handleRequest($request);
+    //
+    //    if ($form->isSubmitted() && $form->isValid()) {
+    //      $product = $form->getData();
+    //
+    //      // $file stores the uploaded file
+    //      $file = $product->getImage();
+    //
+    //      $fileName = $this->get('app.image')->move($file);
+    //
+    //      // Update the 'avatar' property to store file name
+    //      // instead of its contents
+    //      $product->setImage($fileName);
+    //
+    //      $product->setUser($this->getUser());
+    //
+    //      $em = $this->getDoctrine()->getManager();
+    //      $em->persist($product);
+    //      $em->flush();
+    //
+    //      $this->addFlash('success', 'Product created');
+    //
+    //      return $this->redirectToRoute('product_list');
+    //    }
+    //
+    //    return $this->render('product/edit.html.twig', ['productForm' => $form->createView(), 'imageName' => null]);
   }
 }
