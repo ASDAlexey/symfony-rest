9f328c1ffa76c67f33d6dae0b645705f2665a568
Alexey Popov <alexey.popov@umbrella-web.com>
2017-11-26 23:31:17 +0300

    made auto-login endpoint

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use AppBundle\Form\AutoLoginForm;
 use AppBundle\Form\LoginForm;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -29,12 +30,65 @@ class SecurityController extends BaseController {
        */
       $user = $em->getRepository('AppBundle:User')->findOneBy(['email' => $formData['email']]);
       if ($user) {
-        $isValid = $this->get('security.password_encoder')->isPasswordValid($user, $formData['password']);
+        $isValid = $this->get('security.password_encoder')
+                        ->isPasswordValid($user, $formData['password']);
         if (!$isValid) {
           $responseData = ["errors" => 'Unauthorized'];
           return $this->createApiResponse($responseData, 401);
         }
 
+        $token = $this->get('lexik_jwt_authentication.encoder')->encode([
+          'id' => $user->getId(),
+          'username' => $user->getEmail(),
+          'exp' => time() + 3600 * 24 * 2 // 2 days expiration
+        ]);
+
+        $responseData = [
+          "data" => [
+            "id" => $user->getId(),
+            "email" => $user->getEmail(),
+            "createdAt" => $user->getCreatedAt(),
+            "updatedAt" => $user->getUpdatedAt(),
+          ],
+          "meta" => ["token" => $token]
+        ];
+        return $this->createApiResponse($responseData);
+      } else {
+        $responseData = ["errors" => 'Unauthorized'];
+        return $this->createApiResponse($responseData, 401);
+      }
+    } else {
+      $errors = $form->getErrors()->getForm();
+      $responseData = ["errors" => $errors];
+      return $this->createApiResponse($responseData, 400);
+    }
+  }
+
+  /**
+   * @Rest\Post("/api/auto-login")
+   * {
+   *   "token":"token_string",
+   */
+  public function autoLogin(Request $request) {
+    $form = $this->createForm(AutoLoginForm::class);
+    $form->submit($request->request->all());
+
+    if ($form->isValid()) {
+      $formData = $form->getData();
+
+      $jwtData = $this->get('lexik_jwt_authentication.encoder')->decode($formData['token']);
+      if (!($jwtData && isset($jwtData['id']))) {
+        $responseData = ["errors" => "Invalid token"];
+        return $this->createApiResponse($responseData, 400);
+      }
+
+      $em = $this->getDoctrine()->getManager();
+
+      /**
+       * @var User $user
+       */
+      $user = $em->getRepository('AppBundle:User')->find($jwtData['id']);
+      if ($user) {
         $token = $this->get('lexik_jwt_authentication.encoder')->encode([
           'id' => $user->getId(),
           'username' => $user->getEmail(),
